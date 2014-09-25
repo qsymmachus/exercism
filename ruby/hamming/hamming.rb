6250d9a02ada17ca0e630cb5825b619ee870259a
John Olmsted <johnaolmsted@gmail.com>
2014-09-25 09:57:46 -0700

    Removes unnecessary conversion of DNA strand strings into arrays

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,7 +2,6 @@ class Hamming
   class << self
     def compute(a, b)
       hamming_diff = 0
-      a, b = a.split(""), b.split("")
       a.length.times do |i|
         break if a[i].nil? || b[i].nil?
         hamming_diff += 1 unless a[i] == b[i]
