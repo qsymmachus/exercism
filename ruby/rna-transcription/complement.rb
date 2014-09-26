0bb327ee91d8ef80f28499d5ea03500085195c57
John Olmsted <johnaolmsted@gmail.com>
2014-09-26 15:06:57 -0700

    Third iteration of /ruby/rna-transcription with the magic of String#tr

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,23 +1,11 @@
 module Complement
-  DNA_COMPLEMENTS = {'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U'}
-  RNA_COMPLEMENTS = DNA_COMPLEMENTS.invert
-
   class << self
     def of_dna(dna)
-      nucleotide_complement(dna, DNA_COMPLEMENTS)
+      dna.tr("GCTA", "CGAU")
     end
 
     def of_rna(rna)
-      nucleotide_complement(rna, RNA_COMPLEMENTS)
-    end
-
-    private
-
-    def nucleotide_complement(strand, complements)
-      strand.length.times do |i|
-        strand[i] = complements[strand[i]]
-      end
-      strand
+      rna.tr("CGAU", "GCTA")
     end
   end
 end
