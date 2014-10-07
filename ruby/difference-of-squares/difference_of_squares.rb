27c25e17bdd36487ce479591cd584179d23c843b
John Olmsted <johnaolmsted@gmail.com>
2014-10-07 12:09:39 -0700

    Second iteration of 'difference of squares' using #inject

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,7 +1,7 @@
 class Squares
   def initialize(num)
     @square_of_sums = (1..num).reduce(:+)**2
-    @sum_of_squares = (1..num).map { |n| n**2 }.reduce(:+)
+    @sum_of_squares = (1..num).inject { |sum, n| sum + n**2 }
     @difference =  @square_of_sums - @sum_of_squares
   end
 
