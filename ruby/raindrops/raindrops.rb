4f884b551fc0f89a5e45fd89e2db98a0bccdc819
John Olmsted <johnaolmsted@gmail.com>
2014-10-07 12:05:00 -0700

    Adds initial solution to 'difference of squares'

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,21 +1,15 @@
-require 'prime'
-
 module Raindrops
   class << self
     def convert(num)
-      factors = prime_factors(n).select { |n| [3,5,7].include?(n) }
-      return num.to_s if factors.empty?
-      plingify(factors.join(""))
-    end
-
-    private
-
-    def prime_factors(num)
-      Prime.each(num).collect { |prime| prime if num % prime == 0 }.compact
-    end
-
-    def plingify(string)
-      string.gsub(/[357]/, "3" => "Pling", "5" => "Plang", "7" => "Plong")
+      string = ""
+      string += "Pling" if num % 3 == 0
+      string += "Plang" if num % 5 == 0
+      string += "Plong" if num % 7 == 0
+      if string.empty?
+        num.to_s
+      else
+        string
+      end
     end
   end
 end
