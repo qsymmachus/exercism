40be35c7d2e690116201d6eec32b6c25ede21ff2
John Olmsted <johnaolmsted@gmail.com>
2014-10-15 16:16:49 -0700

    Second iteration of 'roman numerals'

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -3,21 +3,22 @@ class Fixnum
     num = self
     roman_numeral = ""
     romans = {
-      1 => "I",
-      4 => "IV",
-      5 => "V",
-      9 => "IX",
-      10 => "X",
-      40 => "XL",
-      50 => "L",
-      90 => "XC",
-      100 => "C",
-      400 => "CD",
-      500 => "D",
+      1000 => "M",
       900 => "CM",
-      1000 => "M"
+      500 => "D",
+      400 => "CD",
+      100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I" 
     }
-    romans.keys.reverse.each do |n|
+
+    romans.keys.each do |n|
       roman_numeral += romans[n] * (num / n)
       num = num % n
     end
