34eb2c694fe0369071aa87e31fb4fca0fd81ff89
John Olmsted <johnaolmsted@gmail.com>
2014-11-05 15:28:49 -0800

    Second iteration of 'bob'
    Modularizes 'Bob' so helper functions are not globally scoped.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,32 +1,34 @@
-var Bob = function() {};
-
-Bob.prototype.hey = function(input) {
-  if (isBlank(input)) {
-    return "Fine. Be that way!";
-  } else if (isUpperCase(input)) {
-    return "Whoa, chill out!";
-  } else if (isQuestion(input)) {
-    return "Sure."
-  } else {
-    return "Whatever.";
+var Bob = function() {
+  function isBlank(string) {
+    return /^\s*$/.test(string);
   }
-};
 
-function isBlank(string) {
-  return /^\s*$/.test(string);
-}
+  function isUpperCase(string) {
+    if (!hasLetters(string)) return false;
+    return string === string.toUpperCase() ? true : false; 
+  }
 
-function isUpperCase(string) {
-  if (!hasLetters(string)) return false;
-  return string === string.toUpperCase() ? true : false; 
-}
+  function isQuestion(string) {
+    return string[string.length - 1] === '?' ? true : false;
+  }
 
-function isQuestion(string) {
-  return string[string.length - 1] === '?' ? true : false;
-}
+  function hasLetters(string) {
+    return /[a-zA-Z]/.test(string)
+  }
 
-function hasLetters(string) {
-  return /[a-zA-Z]/.test(string)
+  return {
+    hey: function(input) {
+      if (isBlank(input)) {
+        return "Fine. Be that way!";
+      } else if (isUpperCase(input)) {
+        return "Whoa, chill out!";
+      } else if (isQuestion(input)) {
+        return "Sure."
+      } else {
+        return "Whatever.";
+      }
+    }
+  }
 }
 
 module.exports = Bob;
