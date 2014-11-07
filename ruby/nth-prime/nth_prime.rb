1dd837c380d5317348a1b07bc60cb4a84f4e689f
John Olmsted <johnaolmsted@gmail.com>
2014-11-06 17:02:17 -0800

    Second iteration of 'nth_prime'
    More than 2x faster performance by skipping even numbers (which are never prime).

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,10 +1,12 @@
+require 'benchmark'
+
 module Prime
   class << self
     def nth(n)
       raise ArgumentError if n <= 0
-      prime = Fixnum
+      prime = 2
       i = 2
-      until n == 0
+      until n == 1
         if is_prime?(i)
           prime = i
           n -= 1
@@ -15,10 +17,15 @@ module Prime
     end
 
     def is_prime?(n)
-      (2..n-1).each do |i|
+      return false if n.even?
+      i = 3
+      until i >= n-1
         return false if n % i == 0 
+        i += 2
       end
       true
     end
   end
 end
+
+puts Benchmark.measure { Prime.nth(10001) }
