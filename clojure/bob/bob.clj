6be12dbab44940507640b77d7d44d2e18ebf2ed3
John Olmsted <john@olmsted.io>
2015-05-20 19:17:12 -0700

    Initial solution to clojure/bob

---
+++ clojure/bob/bob.clj
@@ -0,0 +1,24 @@
+(ns bob)
+
+(defn yelling?
+  "Checks whether a string 'yelling'. A string is yelling if:
+    1. It is all upper case,
+    2. It contains at least one letter."
+  [string]
+  (and 
+    (= string (clojure.string/upper-case string)) 
+    (re-find #"[a-zA-Z]" string)))
+
+(defn question?
+  "Checks whether a string is question (that is, whether it ends in '?')."
+  [string]
+  (= \? (last string)))
+
+(defn response-for
+  "Bob the lackadaisical teenager will respond to your statements."
+  [statement]
+  (cond
+    (clojure.string/blank? statement) (str "Fine. Be that way!")
+    (yelling? statement) (str "Whoa, chill out!")
+    (question? statement) (str "Sure.")
+    :else (str "Whatever.")))
