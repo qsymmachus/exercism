a4e76d2c96ff3ca9ac8c4056bcee9951ac058f62
John Olmsted <john@olmsted.io>
2018-05-03 19:53:56 -0700

    Adds initial solution to python/bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,6 +1,21 @@
-#
-# Skeleton file for the Python "Bob" exercise.
-#
-def hey(what)
+import re
 
-    return
+def hey(what):
+    if is_question(what):
+        return 'Sure.'
+    elif is_yelling(what):
+        return 'Whoa, chill out!'
+    elif is_empty(what):
+        return 'Fine. Be that way!'
+    else:
+        return 'Whatever.'
+
+def is_question(what):
+    return re.search(r'\w*\?$', what) and not is_yelling(what)
+
+def is_yelling(what):
+    return re.search(r'[a-zA-Z]+', what) and what.upper() == what and not is_empty(what)
+
+def is_empty(what):
+    return re.search(r'^\s*$', what)
+    
